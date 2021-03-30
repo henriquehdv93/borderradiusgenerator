8bda10f6c6c7d73a85545f067c14f1230a689399
Henrique Vargas <henrique@henriquevargas.com.br>
2021-03-30 04:48:00 -0300

    finished front-end of layout and values

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
-    <header></header>
+    <Header />
+    <main class="appContainer">
+      <SetValues />
+      <section>
+        <div class="appPreview">
+        </div>
+        <div class="appOutput">
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 <script>
+import Header from '../components/Header'
+import SetValues from '../components/SetValues'
 export default {
-  
+  components: {
+    Header,
+    SetValues,
+  }
 }
 </script>
