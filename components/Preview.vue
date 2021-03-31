7b33bccb2f07cb7106dae2fd43e5fbdc8bc98a5a
Henrique Vargas <henrique@henriquevargas.com.br>
2021-03-31 17:27:28 -0300

    finishing the application of border-radius preview

--- components/Preview.vue
+++ components/Preview.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="appPreview">
-    <div class="appPreview--box">
-    Preview
+    <div class="appPreview--box" :style="{ borderRadius: allCorners + 'px' }">
+      Preview
     </div>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 export default {
   data: () => ({
-    
+  }),
+  computed: {
+  ...mapState({
+    allCorners: state => state.allCorners,
+    topLeft: state => state.topLeft,
+    topRight: state => state.topRight,
+    bottomLeft: state => state.bottomLeft,
+    bottomRight: state => state.bottomRight,
   })
+},
 }
 </script>
