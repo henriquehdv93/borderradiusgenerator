d1d8c448023d2ca7cfb104b298dcae7a95b93929
Henrique Vargas <henrique@henriquevargas.com.br>
2021-03-31 20:33:58 -0300

    finished output code

--- components/Output.vue
+++ components/Output.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="appOutput">
-    <code>
+    <code v-if="isMultipleValue == true">
       {{ allCorners > 0 ? 'border-radius: ' + allCorners + 'px;' : '' }}
+    </code>
+    <code v-else>
+      {{ outputMultiple == true ? 'border-radius: ' + topLeft + 'px ' + topRight + 'px ' + bottomRight + 'px ' + bottomLeft + 'px;' : '' }}
     </code>
     <button type="button" class="btnDefault">Copy to clipboard</button>
   </div>
@@ -9,16 +12,22 @@
 <script>
 import { mapState } from 'vuex';
 export default {
-  data: () => ({
-    
-  }),
   computed: {
+    outputMultiple() {
+      if (this.topLeft > 0 || this.topRight > 0 || this.bottomRight > 0 || this.bottomLeft > 0 ) {
+        return true;
+      }
+      else {
+        return false
+      }
+    },
     ...mapState({
       allCorners: state => state.allCorners,
       topLeft: state => state.topLeft,
       topRight: state => state.topRight,
       bottomLeft: state => state.bottomLeft,
       bottomRight: state => state.bottomRight,
+      isMultipleValue: state => state.isMultipleValue,
     })
   },
 }
