41ff12d2af263967bb13271aa4061b30269812c2
Henrique Vargas <henrique@henriquevargas.com.br>
2021-03-31 22:33:56 -0300

    added link github

--- components/Header.vue
+++ components/Header.vue
@@ -2,16 +2,18 @@
   <header class="title">
     <h1>{{ title }}</h1>
     <p>Desenvolvido por: <a :href="developedBy.url" rel="nofollow" target="_blank">{{ developedBy.name }}</a></p>
+    <p><a :href="github" rel="nofollow" target="_blank">Acesse o Github do projeto</a></p>
   </header>
 </template>
 <script>
 export default {
   data: () => ({
-      title: 'Border Radius Generator',
+      title: 'Gerador de CSS3 border-radius',
       developedBy: {
         name: 'Henrique Vargas',
         url: 'https://henriquevargas.com.br',
-      }
+      },
+      github: 'https://github.com/henriquehdv93/borderradiusgenerator/'
   })
 }
 </script>
