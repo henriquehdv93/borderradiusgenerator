5bfada58cb138eb99906be6cd5c9efbdb24635de
Henrique Vargas <henrique@henriquevargas.com.br>
2021-04-01 15:35:43 -0300

    added footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <a :href="logo.url" :title="logo.title" target="_blank" rel="nofollow">
+      <img :src="logo.src" :alt="logo.alt" :title="logo.title">
+    </a>
+  </footer>
+</template>
+<script>
+export default {
+  data: () => ({
+    logo: {
+      url: 'https://www.henriquevargas.com.br',
+      src: 'images/henriquevargas.png',
+      title: 'Henrique Vargas - Desenvolvedor web',
+      alt: 'Desenvolvido por Henrique Vargas'
+    }
+  })
+}
+</script>
